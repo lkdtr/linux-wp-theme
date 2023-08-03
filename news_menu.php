9edbedbb2f96ddf2b968c828c9898ded69c332e2
Bahri Meriç Canlı <bahri@bahri.info>
2023-08-03 17:54:22 +0300

    menu

--- news_menu.php
+++ news_menu.php
@@ -8,7 +8,7 @@
   </select>
   <h2 class="content-news">Haberler</h2>
   <p>
-    <a href="/haberler/yeni-haber-girisi">Yeni Haber Gir</a>
+    <a href="/yeni-haber-girisi">Yeni Haber Gir</a>
     <a href="/haberler">Tüm Haberleri Gör</a>
   </p>
 </div>
