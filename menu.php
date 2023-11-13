13b098dceb474678c01772e5bbc1c99fcfbbe1f2
Bahri Meriç Canlı <bahri@bahri.info>
2023-11-13 14:06:46 +0300

    nav

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <div class="wrapper" id="horizontal_menu">
-  <?php wp_nav_menu( array( 'container' => 'ul', 'theme_location' => 'menu', 'items_wrap' => '<ul id="menu-header">%3$s</ul>', 'menu_id' => 'nav')); ?>
-  <?php wp_nav_menu( array( 'container' => 'ul', 'theme_location' => 'menu_en', 'items_wrap' => '<ul id="menu-header">%3$s</ul>', 'menu_id' => 'nav_en')); ?>
+  <?php wp_nav_menu( array( 'container' => 'ul', 'theme_location' => 'menu', 'items_wrap' => '<ul id="nav">%3$s</ul>', 'menu_id' => 'nav')); ?>
+  <?php wp_nav_menu( array( 'container' => 'ul', 'theme_location' => 'menu_en', 'items_wrap' => '<ul id="nav_en">%3$s</ul>', 'menu_id' => 'nav_en')); ?>
 </div>
 <div class="clear"></div>
